2e332fce2844047a740e029594402e300cd5c63c
Bruno dos Reis Leal <bdrleal@gmail.com>
2025-10-31 00:37:33 +0000

    modificada a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Em Minas tudo é melhor</h1>
+    return <h1>Meu Zão, eu serei um progamador, e sempre contei com o seu apoio Obrigado.</h1>
 }
 
 export default Home;
