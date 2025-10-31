e9287626328f6e743c11902e222f5158bbcd54b3
Bruno dos Reis Leal <bdrleal@gmail.com>
2025-10-31 00:45:04 +0000

    correção do texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Meu Zão, eu serei um progamador, e sempre contei com o seu apoio Obrigado.</h1>
+    return <h1>Meu Zão, eu serei um programador, e sempre conto com o seu apoio... Obrigado.</h1>
 }
 
 export default Home;
